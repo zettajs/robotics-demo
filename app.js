b788335a362acbc928c460a3af3bcdaacefc7f86
Adam Magaluk <AdamMagaluK@gmail.com>
2014-05-06 12:39:48 -0400

    Added button logic

--- app.js
+++ app.js
@@ -6,6 +6,25 @@ var HelloApp = module.exports = function() {
 HelloApp.prototype.init = function(zetta) {
 
   zetta
+     .observe('type="button"')
+     .zip(zetta.observe('type="arm"'), zetta.observe('type="huehub"'), zetta.observe('type="screen"'))
+     .first()
+     .subscribe(function(devices){
+       var button = devices[0];
+       var arm = devices[1];
+       var huehub = devices[2];       
+       var screen = devices[3];
+       
+       button.on('click',function(){
+	 arm.call('pivot-right');
+	 screen.call('change','Button Pressed!!!');
+	 if(huehub.registerd)
+	   huehub.call('blink');
+       });
+     });
+
+
+  zetta
     .observe('type="screen"')
     .zip(zetta.observe('type="arm"'), zetta.observe('type="huehub"'))
     .first()
