6753e9bbf75ccf2b89387c766c2009db8521086a
Adam Magaluk <AdamMagaluK@gmail.com>
2014-05-08 12:45:34 -0400

    Added push notifications

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 var HueScout = require('zetta-hue-driver');
+var PushNotificationScout = require('zetta-apigee-driver');
 
 module.exports = function(runtime) {
   runtime.scouts.push(HueScout);
+  runtime.scouts.push(PushNotificationScout);
 };
